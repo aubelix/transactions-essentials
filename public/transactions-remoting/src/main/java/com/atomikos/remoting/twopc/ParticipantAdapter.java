164467da91669e5c9b9ca70c436ed61e7dd7320d
aubelm <martin.aubele.ext@msg.group>
2023-08-05 15:36:25 +0200

    Change default for readTimout to 60 seconds, 10 seconds was too short.

--- public/transactions-remoting/src/main/java/com/atomikos/remoting/twopc/ParticipantAdapter.java
+++ public/transactions-remoting/src/main/java/com/atomikos/remoting/twopc/ParticipantAdapter.java
@@ -30,6 +30,8 @@ import com.atomikos.icatch.HeurRollbackException;
 import com.atomikos.icatch.Participant;
 import com.atomikos.icatch.RollbackException;
 import com.atomikos.icatch.SysException;
+import com.atomikos.icatch.config.Configuration;
+import com.atomikos.icatch.provider.ConfigProperties;
 import com.atomikos.logging.Logger;
 import com.atomikos.logging.LoggerFactory;
 import com.atomikos.remoting.support.HeaderNames;
@@ -52,9 +54,27 @@ public class ParticipantAdapter implements Participant {
 	public ParticipantAdapter(URI uri) {
 		if (client == null) {
 			ResteasyClientBuilder builder = new ResteasyClientBuilder();
-			builder.connectTimeout(2, TimeUnit.SECONDS);
-			builder.readTimeout(10, TimeUnit.SECONDS);
-			Client c = builder.connectionPoolSize(20).build(); 
+			
+			ConfigProperties configProperties = Configuration.getConfigProperties();
+			String connectionPoolSizeProperty = configProperties.getProperty("com.atomikos.remoting.twopc.ParticipantAdapter.connectionPoolSize");
+			int connectionPoolSize = 20;
+			if (connectionPoolSizeProperty != null)
+				connectionPoolSize = Integer.valueOf(connectionPoolSizeProperty);
+			
+			String connectTimeoutProperty = configProperties.getProperty("com.atomikos.remoting.twopc.ParticipantAdapter.connectTimeout");
+			int connectTimeout = 10;
+			if (connectTimeoutProperty != null)
+				connectTimeout = Integer.valueOf(connectTimeoutProperty);
+
+
+			String readTimeoutProperty = configProperties.getProperty("com.atomikos.remoting.twopc.ParticipantAdapter.readTimeout");
+			int readTimeout = 60;
+			if (readTimeoutProperty != null)
+				readTimeout = Integer.valueOf(readTimeoutProperty);
+
+			builder.connectTimeout(connectTimeout, TimeUnit.SECONDS);
+			builder.readTimeout(readTimeout, TimeUnit.SECONDS);
+			Client c = builder.connectionPoolSize(connectionPoolSize).build(); 
 			c.property("jersey.config.client.suppressHttpComplianceValidation", true);
 			c.register(ParticipantsProvider.class);
 			client = c;
